b153233e2e009782f7705249714cf120f5f6a611
Blue Eu <euisblue@pm.me>
2021-10-15 14:18:41 +0900

    random color palette finished

--- 02-random-color-palette/js/palette.js
+++ 02-random-color-palette/js/palette.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const palettes = document.querySelectorAll('.palette');
-    
+    const copiedInfo = document.querySelector('.info');
+
     palettes.forEach((palette) => {
-        palette.style.backgroundColor = "#00F";
+        // let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        let randomColor = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+        palette.style.backgroundColor = `#${randomColor}`;
+        console.log('color: ', randomColor);
+
+        palette.addEventListener('click', (e) => {
+            navigator.clipboard.writeText(randomColor);
+            copiedInfo.classList.remove('hide');
+            setTimeout(function() {
+                copiedInfo.classList.add('hide');
+            }, 1000);
+        })
     });
 });
