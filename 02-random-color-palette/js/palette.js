d433a0044dc0858a570584dbc62733881557ca2b
Blue Eu <euisblue@pm.me>
2021-10-17 22:34:24 +0900

    transition added and gif updated

--- 02-random-color-palette/js/palette.js
+++ 02-random-color-palette/js/palette.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const palettes = document.querySelectorAll('.palette');
-    const copiedInfo = document.querySelector('.info');
+    regen();
+    function regen() {
+        const palettes = document.querySelectorAll('.palette');
+        const copiedInfo = document.querySelector('.info');
+        
+        palettes.forEach((palette) => {
+            let randomColor = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+            palette.style.backgroundColor = `#${randomColor}`;
+            console.log('color: ', randomColor);
+            
+            palette.addEventListener('click', (e) => {
+                navigator.clipboard.writeText(randomColor);
+                copiedInfo.classList.remove('hide');
+                setTimeout(function() {
+                    copiedInfo.classList.add('hide');
+                }, 1000);
+            })
+        });
+    }
 
-    palettes.forEach((palette) => {
-        // let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        let randomColor = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
-        palette.style.backgroundColor = `#${randomColor}`;
-        console.log('color: ', randomColor);
+    const generateButton = document.querySelector('.generate-btn');
 
-        palette.addEventListener('click', (e) => {
-            navigator.clipboard.writeText(randomColor);
-            copiedInfo.classList.remove('hide');
-            setTimeout(function() {
-                copiedInfo.classList.add('hide');
-            }, 1000);
-        })
-    });
+    generateButton.addEventListener('click', regen);
 });
